15985b983b2991b69dfb9a3942d2943b39b6b223
root <root@ovirt-gofen-1.scl.lab.tlv.redhat.com>
2014-11-24 17:33:30 +0200

    new

--- Sanity/iSCSi_Connections/iSCSi_Connections.py
+++ Sanity/iSCSi_Connections/iSCSi_Connections.py
@@ -3,6 +3,8 @@ import os
 from ovirtsdk.api import API
 from ovirtsdk.xml import params
 
+
+
 def Connect():
     """ This Function connects To our engine,classes and db
     """
@@ -12,51 +14,61 @@ def Connect():
     password = 'qum5net'
     return API(url=u, password=password, username=user, insecure=True)
 
-def get_Engine_ListofLuns(lun_list):
-    """ Gets dict name lun_dict, and return it,initialized with logical
-    unit object that correspond with host's address (which is a string)
-    """
-    api = Connect()
-    for Host in api.hosts.list():
-        lun_list[Host.get_address()]=list()
-        for Storage in Host.storage.list():
-            lun_list[Host.get_address()].append(Storage.get_logical_unit())
+class iSCSiConnections():
+    ''' class to check iSCSi connections
+    '''
 
-def get_HostIqnList(host_name):
-    """ Gets an empty list and returns it full with hosts
-    iqn strings
-    """
 
-    iqn = list()
-    with open('/root/Sanity/%s' %host_name,'r') as f:
-        while True:
-            line = f.readline()
-            if line == '':
-                break
-            elif re.search('flash', re.split(' ',line)[-1], flags = 1) != None:
-                iqn.append(re.split(' ',line)[-2])
-                continue
-            iqn.append(re.split(' ',line)[-1])
-            iqn[-1] = iqn[-1].rsplit()
-    return iqn
+    def __init__(self):
+        self.api = Connect()
+        self.storageconnections = list()
+        self.hostsIqn = dict()
+        self.hostsIp = list()
 
-def get_HostsNames(api,names):
-    """ Gets oVirt's api and an empty list and returns it full with hosts names
-    """
+    def get_HostIqnList(self,host_address):
+        """ Gets an empty list and returns it full with hosts
+        iqn strings
+        """
+        os.system("./session.ini %s" % host_address)
+        iqn = list()
+        with open('/tmp/%s' %host_address,'r') as f:
+            while True:
+                line = f.readline()
+                if line == '':
+                    break
+                elif re.search('flash', re.split(' ',line)[-1], flags = 1) != None:
+                    iqn.append(re.split(' ',line)[-2])
+                    continue
+                iqn.append(re.split(' ',line)[-1])
+                iqn[-1] = iqn[-1].rsplit()
+        return iqn
 
-    HOSTSList = api.hosts.list()
-    for HOSTS in HOSTSList:
-        print HOSTS.get_name()
-        names.append(HOSTS.get_name())
+    def start(self):
+        """ Gets dict name lun_dict, and return it,initialized with logical
+        unit object that correspond with host's address (which is a string)
+        """
+        for Host in self.api.hosts.list():
+            self.hostsIp.append(Host.get_address())
+            self.hostsIqn[Host.get_address()] = list()
+            self.hostsIqn[Host.get_address()] = self.get_HostIqnList(Host.get_address())
+        for Storage in self.api.storagedomains.get("iSCSi_1").storageconnections.list():
+            self.storageconnections.append(Storage.get_target())
+
+    def compareHosts(self):
+        for i in range(0,len(self.hostsIp)-1):
+            for j in range(1,len(self.hostsIp) -i):
+                flag = cmp(self.hostsIqn[self.hostsIp[i]],self.hostsIqn[self.hostsIp[i+j]])
+                if flag == 1:
+                    return len(self.hostsIqn[self.hostsIp[i]]) == len(self.hostsIqn[self.hostsIp[i+j]])
+                else:
+                    return flag
+
+ #   def compareHostsTodb(self):
 
 if __name__ == "__main__":
-    api = Connect()
-    names=list()
-    get_HostsNames(api,names)
-    for n in names:
-        os.system("./check-iSCSiConnections %s" % n)
-        iq = get_HostIqnList(n)
-        print iq
+    connection = iSCSiConnections()
+    connection.start()
+    print connection.compareHosts()
 #    print len(iqn)
 #    print iqn
 #    get_IqnList(iqn)
